ec42e52205a8e7d3c43ed088e18ca5f315ff0781
Dorgo_TXT <165108758+DorgoTxt@users.noreply.github.com>
2024-04-30 15:36:23 +0000

    Primer dinamico por /

--- src/aside.js
+++ src/aside.js
@@ -2,7 +2,7 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
-function Formulario() {
+function Aside() {
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
@@ -27,29 +27,4 @@ function Formulario() {
   );
 }
 
-export default Formulario;
-// const Aside = () => {
-//     return (
-//         <aside>
-//             <div className="container mt-4">
-//                 <h2>Registrate</h2>
-//             </div>
-//             <form>
-//                 <div className="mb-3">
-//                     <label htmlFor="nombre" className="form-label">Nombre</label>
-//                     <input type="text" className="form-control" id="nombre" placeholder="Jose" required />
-//                 </div>
-//                 <div className="mb-3">
-//                     <label htmlFor="email" className="form-label">Email</label>
-//                     <input type="email" className="form-control" id="email" placeholder="ejemplo@dominio.com" required />
-//                 </div>
-//                 <div className="mb-3">
-//                     <label htmlFor="password" className="form-label">Contraseña</label>
-//                     <input type="password" className="form-control" id="password" placeholder="contraseña123" required />
-//                 </div>
-//                 <button type="submit" className="btn btn-primary">Registrarse</button>
-//             </form>
-//         </aside>
-//     );
-// }
-// export default Aside;
+export default Aside;
--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
 import { createRoot } from "react-dom/client";
 import React, { StrictMode } from "react";
-import Message from "./message";
-import Header from "./header";
-import Main from "./main";
-import Aside from "./aside";
-import Footer from "./footer";
-import Carrosuel from "./carrousel";
 import "./styles.css";
-import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Carrouselfotos from "./carrousel";
 const feather = require("feather-icons");
+import ReactDOM from 'react-dom';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Single from "./single";
+import Basic from "./basic";
 
 setTimeout(() => {
-    feather.replace();
+  feather.replace();
 }, 1000);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Basic />,
+  },
+  {
+    path: "detalle",
+    element: <Single />,
+  },
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+
+);
 
 const root = createRoot(document.getElementById("root"));
 root.render(
-    <StrictMode>
-        <Header></Header>
 
-        <div className="container">
-            <div className="row">
-                <div className="col-md-8 py-4">
-                    <div className="row">
-                        <Main></Main>
-                        <Carrouselfotos></Carrouselfotos>
-                    </div>
-                </div>
-                <div className="col-md-4 py-5">
-                    <Aside></Aside>
-                </div>
-            </div>
-        </div>
-        <Footer></Footer>
-    </StrictMode>
+  <RouterProvider router={router} />
 );
